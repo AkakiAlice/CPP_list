cb3c317a1010898d050115ce2e5652fffa6ac59c
Alice Akaki <akakialice@gmail.com>
2023-08-01 11:54:35 -0400

    refactor: ex00

--- cpp_02/ex00/Fixed.cpp
+++ cpp_02/ex00/Fixed.cpp
@@ -2,18 +2,15 @@
 
 Fixed::Fixed(void) : _fixedValue(0) {
 	std::cout << "Default constructor called" << std::endl;
-	return;
 }
 
 Fixed::Fixed(Fixed const & src) {
 	std::cout << "Copy constructor called" << std::endl;
 	*this = src;
-	return;
 }
 
 Fixed::~Fixed(void) {
 	std::cout << "Destructor called" << std::endl;
-	return;
 }
 
 Fixed&	Fixed::operator=(Fixed const & rhs) {
